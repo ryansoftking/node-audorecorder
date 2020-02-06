320c031c18cc1096b35b45924bb917aa00015bd5
ryansoftking <ryansoftking@gmail.com>
2020-02-05 18:54:06 -0800

    final push from the local

---
+++ examples/print-command.js
@@ -0,0 +1,7 @@
+// Module.
+const AudioRecorder = require('../library');
+
+// Initialize recorder.
+new AudioRecorder({
+	program: process.platform === 'win32' ? 'sox' : 'rec'
+}, console);
---
+++ test/options.js
@@ -0,0 +1,98 @@
+// Dependency modules.
+const test = require('ava');
+// Custom module.
+const AudioRecorder = require('../library');
+
+// Default arecord options.
+test('arecord', function(t) {
+	const audioRecorder = new AudioRecorder({
+		program: 'arecord'
+	});
+	t.deepEqual(audioRecorder.options.program, 'arecord');
+	t.deepEqual(audioRecorder.command.arguments, [
+		'-q',
+		'-c',
+		'1',
+		'-r',
+		'16000',
+		'-t',
+		'wav',
+		'-f',
+		'S16_LE'
+	]);
+	t.deepEqual(audioRecorder.command.options, {
+		encoding: 'binary'
+	});
+});
+
+// Default options (rec).
+test('default', function(t) {
+	const audioRecorder = new AudioRecorder();
+	t.deepEqual(audioRecorder.options.program, 'rec');
+	t.deepEqual(audioRecorder.command.arguments, [
+		'-q',
+		'-c',
+		'1',
+		'-r',
+		'16000',
+		'-t',
+		'wav',
+		'-V0',
+		'-L',
+		'-b',
+		'16',
+		'-e',
+		'signed-integer',
+		'-',
+		'silence',
+		'-l',
+		'1',
+		'0.1',
+		'0.5%',
+		'1',
+		'2.0',
+		'0.5%'
+	]);
+	t.deepEqual(audioRecorder.command.options, {
+		encoding: 'binary'
+	});
+});
+
+// Default sox options.
+test('sox', function(t) {
+	const audioRecorder = new AudioRecorder({
+		program: 'sox'
+	});
+	t.deepEqual(audioRecorder.options.program, 'sox');
+	const expectedArguments = [
+		'-q',
+		'-c',
+		'1',
+		'-r',
+		'16000',
+		'-t',
+		'wav',
+		'-V0',
+		'-L',
+		'-b',
+		'16',
+		'-e',
+		'signed-integer',
+		'-',
+		'silence',
+		'-l',
+		'1',
+		'0.1',
+		'0.5%',
+		'1',
+		'2.0',
+		'0.5%'
+	];
+	if (process.platform === 'win32') {
+		expectedArguments.unshift('-d');
+	}
+	t.deepEqual(audioRecorder.command.arguments, expectedArguments);
+	t.deepEqual(audioRecorder.command.options, {
+		encoding: 'binary'
+	});
+});
